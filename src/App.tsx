124b6f171aabcd4c1ceeaf39ced814f74f40bbbd
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-12-05 01:06:28 +0700

    create hero, enough for today :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Item from './components/Item'
 
 function App() {
   return (
     <div className="App">
       <Header /> 
       <Hero />
+      <Item />
     </div>
   );
 }
