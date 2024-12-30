aae6478db81786cee487af3f74b10bdb5f1aef18
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-12-30 15:00:03 +0700

    little adding

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Item from './components/Item'
+import ItemList from './components/item-list';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header /> 
       <Hero />
       <Item />
+      <ItemList />
     </div>
   );
 }
